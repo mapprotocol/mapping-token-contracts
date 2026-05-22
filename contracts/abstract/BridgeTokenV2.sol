3f12cf0042eecdf36b78e51233bba966b1c5ed9f
Phil <philllllau@gmail.com>
2026-05-22 19:33:24 +0800

    update openzeppelin to 4.9.6.

--- contracts/abstract/BridgeTokenV2.sol
+++ contracts/abstract/BridgeTokenV2.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.20;
 
 import { AccessControlEnumerable } from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import { IBridgeTokenV2 } from "../interface/IBridgeTokenV2.sol";
 
@@ -79,7 +78,7 @@ abstract contract BridgeTokenV2 is ERC20Pausable, AccessControlEnumerable, IBrid
         address from,
         address to,
         uint256 amount
-    ) internal virtual override(ERC20, ERC20Pausable) {
+    ) internal virtual override(ERC20Pausable) {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,10 +3,8 @@ import * as dotenv from "dotenv";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
-import 'hardhat-dependency-compiler'
 // import "@nomicfoundation/hardhat-verify";
 import 'hardhat-storage-layout';
-import "hardhat-gas-reporter";
 import "solidity-coverage";
 import 'hardhat-deploy';
 require('./tasks');
@@ -37,24 +35,24 @@ const config: HardhatUserConfig = {
       chainId: 212,
       url:"https://testnet-rpc.maplabs.io",
       zksync: false,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts: process.env.TESTNET_PRIVATE_KEY !== undefined ? [process.env.TESTNET_PRIVATE_KEY] : [],
     },
     Map: {
       chainId: 22776,
       url: "https://rpc.maplabs.io",
-      accounts: process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Polygon: {
       chainId: 137,
       url: "https://rpc-mainnet.matic.quiknode.pro",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    bsc: {
+    Bsc: {
       chainId: 56,
       url: "https://bsc-dataseed.binance.org/",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    eth: {
+    Eth: {
       chainId: 1,
       url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
@@ -62,29 +60,18 @@ const config: HardhatUserConfig = {
     Merlin: {
       url: `https://rpc.merlinchain.io`,
       chainId : 4200,
-      accounts: process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
-    },
-
-    Bevm: {
-      url: `https://rpc-canary-2.bevm.io/`,
-      chainId : 1501,
-      accounts: process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
-    },
-
-    Gnosis: {
-      url: `https://1rpc.io/gnosis`,
-          chainId : 100,
-          accounts: process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
+      gasPrice: 50000000,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Sepolia: {
       url: `https://1rpc.io/sepolia`,
       chainId : 11155111,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts: process.env.TESTNET_PRIVATE_KEY !== undefined ? [process.env.TESTNET_PRIVATE_KEY] : [],
     },
     Holesky: {
       url: `https://1rpc.io/holesky`,
       chainId : 17000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts: process.env.TESTNET_PRIVATE_KEY !== undefined ? [process.env.TESTNET_PRIVATE_KEY] : [],
     },
 
   },
@@ -95,25 +82,15 @@ const config: HardhatUserConfig = {
   //   // Optional: specify a different Sourcify repository
   //   browserUrl: "https://repo.sourcify.dev",
   // },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  dependencyCompiler: {
-    paths: [
-      '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol',
-    ],
-    keep: true,
-  },
   etherscan: {
     apiKey: {
-      polygon: "JT3V5H3AZRFKEBK8W8NGWA7G17AR3IQC2W",
-      bsc: "JT3V5H3AZRFKEBK8W8NGWA7G17AR3IQC2W",
-      eth: "JT3V5H3AZRFKEBK8W8NGWA7G17AR3IQC2W"
+      Polygon: process.env.ETHERSCAN_API_KEY || "",
+      Bsc: process.env.ETHERSCAN_API_KEY || "",
+      Eth: process.env.ETHERSCAN_API_KEY || ""
     },
     customChains: [
       {
-        network: "eth",
+        network: "Eth",
         chainId: 1,
         urls: {
           apiURL: "https://api.etherscan.io/v2/api?chainid=1",
@@ -121,7 +98,7 @@ const config: HardhatUserConfig = {
         }
       },
       {
-        network: "polygon",
+        network: "Polygon",
         chainId: 137,
         urls: {
           apiURL: "https://api.etherscan.io/v2/api?chainid=137",
@@ -129,7 +106,7 @@ const config: HardhatUserConfig = {
         }
       },
       {
-        network: "bsc",
+        network: "Bsc",
         chainId: 56,
         urls: {
           apiURL: "https://api.etherscan.io/v2/api?chainid=56",
